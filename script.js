5d9a2b75939989fca27293d68da3565fab93ba13
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2024-01-08 19:49:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	let random1="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+	let res='';
+	for(let i=0;i<l;i++){
+		let index=Math.floor(Math.random() * random1.length);
+		res+=random1.charAt(index);
+	}
+	return res;
 }
 
 // Do not change the code below.
